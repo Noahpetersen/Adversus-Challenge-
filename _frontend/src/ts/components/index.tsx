c840547182f6ccadd377502b6333f0fb9dad9752
Christian Johannesen <>
2020-02-02 19:16:49 +0100

    Added basic sidebar

--- _frontend/src/ts/components/index.tsx
+++ _frontend/src/ts/components/index.tsx
@@ -2,34 +2,38 @@ import * as React from 'react';
 import { RecentSalesView } from './views/recent-sales';
 import { TopSalesView } from './views/top-sales';
 import { SplashModal } from './widgets/splash-modal';
+import { Sidebar } from './views/sidebar';
 
 interface State {
-    mode: 'top' | 'recent';
-    splash: boolean;
-    // ...
+	mode: 'top' | 'recent';
+	splash: boolean;
+	// ...
 }
 export class DashboardApplication extends React.Component<{}, State> {
-    state: State = {
-        mode: 'top',
-        splash: true
-    }
-    componentDidMount() {
-        // initialize services
-        // ...
-    }
-    render() {
-        return (
-            <div className="container" >
-                <h1>Dashify</h1>
-                {this.state.mode === 'recent' ?
-                    <RecentSalesView />
-                    : <TopSalesView />
-                }
-                {this.state.splash ?
-                    <SplashModal />
-                    : null
-                }
-            </div>
-        )
-    }
+	state: State = {
+		mode: 'top',
+		splash: true
+	}
+	componentDidMount() {
+		// initialize services
+		// ...
+	}
+	render() {
+		return (
+			<>
+				<Sidebar />
+				<div className="main">
+					<h1>Dashify</h1>
+					{this.state.mode === 'recent' ?
+						<RecentSalesView />
+						: <TopSalesView />
+					}
+					{this.state.splash ?
+						<SplashModal />
+						: null
+					}
+				</div>
+			</>
+		)
+	}
 }
